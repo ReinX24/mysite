2d86fab6f95e932c0b2af47fcb85dcf4b33c187b
rein <reinX244@gmail.com>
2024-01-07 22:09:26 +0800

    Added HTML for default page.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def post_list(request):
+    """Return a HTML template of our posts."""
+    return render(request, "blog/post_list.html", {})
