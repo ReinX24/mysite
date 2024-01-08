58d1ce700cb6fd8c7fb75571598105225b9334a2
rein <reinX244@gmail.com>
2024-01-09 02:24:04 +0800

    Re-added static files.

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#6mma#9)jz6qf%yz3pvxv6ngu_#f=a^8gyjik%ou3&z0xlm3g)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "reinx24.pythonanywhere.com"]
 
@@ -121,7 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     "/var/www/static/",
+# ]
+STATIC_ROOT = BASE_DIR / "blog" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
