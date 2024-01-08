d7b4aa557d8d81a740e574cea004dba779fba94e
rein <reinX244@gmail.com>
2024-01-08 22:27:11 +0800

    Styled post_list page and added post_detail page.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from blog.models import Post
 
@@ -10,3 +10,9 @@ def post_list(request):
         "published_date"
     )
     return render(request, "blog/post_list.html", {"posts": posts})
+
+
+def post_detail(request, post_id):
+    """Return a page with post details."""
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, "blog/post_detail.html", {"post": post})
